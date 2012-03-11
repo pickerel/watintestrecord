d980557f02abcb725e54aecbacb7d36b7ed48def
ddwyer <ddwyer@cf66a50f-b784-4b83-b412-73b7fe32bf1c>
2012-03-11 05:51:52 +0000

    added some interfaces

---
+++ version3/frmModifyFinder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Specialized;
+using System.Windows.Forms;
+using TestRecorder.Core.Actions;
+
+namespace TestRecorder
+{
+    public partial class frmModifyFinder : Form
+    {
+        private NameValueCollection _finder;
+
+        public frmModifyFinder()
+        {
+            InitializeComponent();
+        }
+
+        public void SetCheckList(ActionElementBase element)
+        {
+            _finder = element.AllAttributes;
+            clbAttributes.Items.Clear();
+            foreach (string attributekey in _finder.AllKeys)
+            {
+                clbAttributes.Items.Add(attributekey + " = " + _finder[attributekey], 
+                    element.ActionFinder.KeyExists(attributekey)?CheckState.Checked : CheckState.Unchecked);
+            }
+        }
+
+        public NameValueCollection GetChecked()
+        {
+            var newFinder = new NameValueCollection();
+            foreach (int indexChecked in clbAttributes.CheckedIndices)
+            {
+                string key = _finder.GetKey(indexChecked);
+                newFinder.Add(key, _finder[key]);
+            }
+            return newFinder;
+        }
+    }
+}
